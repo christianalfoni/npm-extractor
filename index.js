136de32b40805c93115055c8a59eeac51ccb1e24
Christian Alfoni <christianalfoni@gmail.com>
2016-02-23 09:52:13 +0100

    Moved test

--- index.js
+++ index.js
@@ -121,38 +121,4 @@ var moduleExport = function (options) {
     })
 };
 
-
-moduleExport({
-  package: 'react',
-  targetFs: mfs,
-  options: {
-    registry: 'http://registry.npmjs.org/',
-    mindelay: 5000,
-    maxDelay: 10000,
-    retries: 5,
-    factor: 5
-  },
-  dest: 'node_modules'
-})
-.then(function () {
-  var fullDir = ['/node_modules'];
-  var logOutDir = function (dir) {
-    var dirs = [];
-    try {
-      dirs = mfs.readdirSync(dir);
-    } catch (e) {
-      return;
-    }
-    dirs.forEach(function (subDir) {
-      fullDir.push(subDir);
-      logOutDir(dir + '/' + subDir);
-      fullDir.pop();
-    });
-  }
-  logOutDir('/node_modules');
-})
-.catch(function (err) {
-  console.log(err);
-})
-
 module.exports = moduleExport;
